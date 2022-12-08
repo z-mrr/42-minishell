73b6833e2f856e5ee495ce0555ea867993369115
zet1r <zet1r@proton.me>
2022-12-08 15:45:58 +0000

    cleanup

--- inc/color.h
+++ inc/color.h
@@ -6,7 +6,7 @@
 /*   By: jdias-mo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/08 15:01:52 by jdias-mo          #+#    #+#             */
-/*   Updated: 2022/12/08 15:20:41 by jdias-mo         ###   ########.fr       */
+/*   Updated: 2022/12/08 15:45:46 by jdias-mo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,16 @@
 # define COLOR_H
 
 //Regular bold text
-#define BLK "\e[1;30m"
-#define RED "\e[1;31m"
-#define GRN "\e[1;32m"
-#define YEL "\e[1;33m"
-#define BLU "\e[1;34m"
-#define MAG "\e[1;35m"
-#define CYN "\e[1;36m"
-#define WHT "\e[1;37m"
+# define BLK "\e[1;30m"
+# define RED "\e[1;31m"
+# define GRN "\e[1;32m"
+# define YEL "\e[1;33m"
+# define BLU "\e[1;34m"
+# define MAG "\e[1;35m"
+# define CYN "\e[1;36m"
+# define WHT "\e[1;37m"
 
 //Reset
-#define DEFAULT "\e[0m"
+# define DEFAULT "\e[0m"
 
 #endif

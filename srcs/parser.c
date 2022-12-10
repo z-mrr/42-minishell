889dda08eaee7b8efd35d497164df4894963cfe3
gde <goncalopes98@gmail.com>
2022-12-10 04:08:37 +0000

    semi funcionar

--- srcs/parser.c
+++ srcs/parser.c
@@ -1,9 +1,26 @@
 #include "../include/minishell.h"
 
-void	ptrExit(char *s)
+/* da free aos **cmds/args */
+/*void	freeCmds(t_frame *f)
 {
-	printf("%s\n", s);
-	exit(-1);
+	int	i;
+
+	i = 0;
+	while (f->cmds->full_cmd[i] != 0)
+		i++;
+	while (i-- >= 0)
+		free(f->cmds->full_cmd[i]);
+	free(f->cmds->full_cmd);
+}*/
+
+int	charArrayLen(char **array)
+{
+	int	i;
+
+	i = 0;
+	while (array[i] != 0)
+		i++;
+	return (i);
 }
 
 /* cria mais um node de cmd */
@@ -14,7 +31,7 @@ void append_dll_cmd(t_frame *f, t_cmd **head)
 
 	new_node = NULL;
 	new_node = (t_cmd *)malloc(sizeof(t_cmd));
-	//new_node->full_cmd = NULL;
+	new_node->full_cmd = NULL;
 	new_node->path = NULL;
 	new_node->in_fd = 0;
 	new_node->out_fd = 1;
@@ -33,30 +50,6 @@ void append_dll_cmd(t_frame *f, t_cmd **head)
 	new_node->prev = last;
 }
 
-int	charArrayLen(char **array)
-{
-	int	i;
-
-	i = 0;
-	while (array[i] != 0)
-		i++;
-	return (i);
-}
-
-
-/* da free aos **cmds/args */
-void	freeCmds(t_frame *f)
-{
-	int	i;
-
-	i = 0;
-	while (f->cmds->full_cmd[i] != 0)
-		i++;
-	while (i-- >= 0)
-		free(f->cmds->full_cmd[i]);
-	free(f->cmds->full_cmd);
-}
-
 /* adiciona mais uma palavra ao cmd/args */
 void	addStrCmd(t_frame *f)
 {
@@ -71,14 +64,14 @@ void	addStrCmd(t_frame *f)
 		f->cmds->full_cmd[1] = 0;
 		return ;
 	}
-	new_cmd = (char **)malloc(sizeof(char *) * (charArrayLen(f->cmds->full_cmd) + 1));
+	new_cmd = (char **)malloc(sizeof(char *) * ((charArrayLen(f->cmds->full_cmd) + 2)));
 	while (f->cmds->full_cmd[i])
 	{
 		new_cmd[i] = ft_strdup(f->cmds->full_cmd[i]);
 		i++;
 	}
 	new_cmd[i] = ft_strdup(f->token->token_str);
-	new_cmd[i] = 0;
+	new_cmd[++i] = 0;
 	free(f->cmds->full_cmd);
 	f->cmds->full_cmd = new_cmd;
 }
@@ -87,4 +80,24 @@ void	addStrCmd(t_frame *f)
 void	parseCmds(t_frame *f)
 {
 	append_dll_cmd(f, &(f->cmds));
+	int	i = 0;
+	while (f->token->next != NULL)
+	{
+		if (f->token->token_type == 'O' && ft_strcmp(f->token->token_str, "| "))
+		{
+			append_dll_cmd(f, &(f->cmds));
+			f->cmds = f->cmds->next;
+			i = 0;
+			f->token = f->token->next;
+			printf("fazer pipe\n");
+		}
+		else
+		{
+		addStrCmd(f);
+		printf("cmd: %s\n", f->cmds->full_cmd[i]);i++;
+		f->token = f->token->next;
+		}
+	}
+	addStrCmd(f);
+	printf("cmd: %s\n", f->cmds->full_cmd[i]);
 }

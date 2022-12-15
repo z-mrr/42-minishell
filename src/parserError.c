f0b4f6989cb3fb6018bd01c5a8308c7ba698c704
gde <goncalopes98@gmail.com>
2022-12-15 18:47:46 +0000

    redirOut

--- src/parserError.c
+++ src/parserError.c
@@ -12,6 +12,14 @@ void	ft_putstr(char *str)
 	}
 }
 
+/*
+void	redirecErros(char *error, int redir)
+{
+	if (redir == 1)
+		write(1, )
+}
+*/
+
 int	parserError(t_sh *f, char *error)
 {
 	if (!(ft_strcmp(error, "\'")) || !(ft_strcmp(error, "\"")))
@@ -22,6 +30,8 @@ int	parserError(t_sh *f, char *error)
 	}
 	else if (!(ft_strcmp(error, "|")))
 		write(1, "minishell: syntax error near unexpected token '|'\n", 50);
+	/*else if (redir != 0)
+		redirecErrors(error, redir);*/
 	if (f->token)
 		freeTokens(f);
 	if (f->cmd)
--- src/redirecs.c
+++ src/redirecs.c
@@ -1,21 +1,93 @@
 #include "../inc/minishell.h"
 
-/* redirec para outfile: '>' '>>'; confirma se pode aceder ao dir (senao devolve erro); dir ou vem no token ou é pwd */
+char	*getFilePath(t_sh *f, t_cmd *node, t_token *token, int mode)
+{
+	char	*path;
+	int	i;
+	char	*tmp;
+
+	path = NULL;
+	if (token->token_str[0] == '/')
+	{
+		i = ft_strlen(token->token_str) - 1;
+		while (i > -1 && token->token_str[i] != '/')
+			i--;
+		path = ft_substr(token->token_str, 0, i + 1);
+	}
+	else
+		path = get_env("PWD", f);
+	if (mode > 0)
+	{
+		if (token->token_str[0] == '/')
+		{
+			free(path);
+			path = ft_strdup(token->token_str);
+			printf("pathname: %s\n", path);
+			return (path);
+		}
+		tmp = ft_strdup(path);
+		free(path);
+		path = ft_strjoin(tmp, token->token_str);
+		free(tmp);
+		printf("pathname: %s\n", path);
+	}
+	return (path);
+}
+
+/* se o file ja existe, ve se csg aceder, senao da erro */
+int	redirecOutFile(char *pathname, t_cmd *node, t_token *token)
+{
+	int	perm;
+
+	perm = 0;
+	if (access(pathname, F_OK) == 0) //existe ficheiro
+	{
+		if (access(pathname, W_OK) == 0)
+		{
+			if (!(ft_strcmp(token->token_str, ">")))
+				node->out_file = open(pathname, O_CREAT | O_RDWR | O_TRUNC, 0644);
+			else
+				node->out_file = open(pathname, O_CREAT | O_RDWR | O_APPEND, 0644);
+			return (0); //no error ?
+		}
+		printf("access denied\n");
+		return (3); //permission denied
+	}
+	if (!(ft_strcmp(token->token_str, ">")))
+		node->out_file = open(pathname, O_CREAT | O_RDWR | O_TRUNC, 0644);
+	else
+		node->out_file = open(pathname, O_CREAT | O_RDWR | O_APPEND, 0644);
+	return (0); //no erro ?
+}
+
+/* ve se dir existe, se conseguir tenta escrever no dir, se sim continua*/
 int	redirOut(t_sh *f, t_cmd *node, t_token *token)
 {
 	DIR	*dir;
 	char	*path;
+	int	i;
 
 	path = NULL;
-	path = getFilePath(f, token, 0);
+	path = getFilePath(f, node, token, 0);
 	printf("path: %s\n", path);
-	if (access(path, F_OK) < 0)
-		{free(path);printf("no such file or dir\n"); return (1);} //error: no such file or dir
-	dir = opendir(path);
-	if (redirOutFile(path, f, node, token))
-		return (1);
-	closedir(dir);
-	return (0); 
+	if (access(path, F_OK) == 0) //path to dir exists
+	{
+		if (access(path, W_OK) == 0)
+		{
+			dir = opendir(path);
+			free(path);
+			path = getFilePath(f, node, token, 1);
+			i = redirecOutFile(path, node, token);
+			closedir(dir);
+			write(node->out_file, "fode te", 7); //loladas !
+			return (i); //se nao for 0, erro
+		}
+		free(path);
+		printf("access denied\n");
+		return (2); //access denied
+	}
+	printf("file nor dir exists\n");
+	return (1); //file or dir not exist
 }
 
 

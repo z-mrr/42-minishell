080cd8674805d6c0a48422c56ae2d94fb262c08e
gde <goncalopes98@gmail.com>
2022-12-15 21:48:00 +0000

    redirOut

--- src/redirecs.c
+++ src/redirecs.c
@@ -1,36 +1,41 @@
 #include "../inc/minishell.h"
 
-char	*getFilePath(t_sh *f, t_cmd *node, t_token *token, int mode)
+/* devolve pathname inteiro do ficheiro */
+char	*getFilePathName(char *path, t_sh *f, t_cmd *node, t_token *token)
+{
+	char	*pathname;
+
+	pathname = NULL;
+	if (token->token_str[0] == '/' || (token->token_str[0] == '.' && token->token_str[1] == '/'))
+	{
+		pathname = ft_strdup(token->token_str);
+		printf("pathname: %s\n", pathname);
+		return (pathname);
+	}
+	pathname = ft_strjoin(path, token->token_str);
+	printf("pathname: %s\n", pathname);
+	return (pathname);
+}
+
+/* devolve path dor dir onde se vai escrever */
+char	*getFilePath(t_sh *f, t_cmd *node, t_token *token)
 {
 	char	*path;
 	int	i;
 	char	*tmp;
 
 	path = NULL;
-	if (token->token_str[0] == '/')
+	if (token->token_str[0] == '/' || (token->token_str[0] == '.' && token->token_str[1] == '/'))
 	{
 		i = ft_strlen(token->token_str) - 1;
 		while (i > -1 && token->token_str[i] != '/')
 			i--;
 		path = ft_substr(token->token_str, 0, i + 1);
+		return (path);
 	}
-	else
-		path = get_env("PWD", f);
-	if (mode > 0)
-	{
-		if (token->token_str[0] == '/')
-		{
-			free(path);
-			path = ft_strdup(token->token_str);
-			printf("pathname: %s\n", path);
-			return (path);
-		}
-		tmp = ft_strdup(path);
-		free(path);
-		path = ft_strjoin(tmp, token->token_str);
-		free(tmp);
-		printf("pathname: %s\n", path);
-	}
+	tmp = get_env("PWD", f);
+	path = ft_strjoin(tmp, "/");
+	free(tmp);
 	return (path);
 }
 
@@ -44,19 +49,19 @@ int	redirecOutFile(char *pathname, t_cmd *node, t_token *token)
 	{
 		if (access(pathname, W_OK) == 0)
 		{
-			if (!(ft_strcmp(token->token_str, ">")))
-				node->out_file = open(pathname, O_CREAT | O_RDWR | O_TRUNC, 0644);
-			else
+			if (!(ft_strcmp(token->prev->token_str, ">>")))
 				node->out_file = open(pathname, O_CREAT | O_RDWR | O_APPEND, 0644);
+			else
+				node->out_file = open(pathname, O_CREAT | O_RDWR | O_TRUNC, 0644);
 			return (0); //no error ?
 		}
 		printf("access denied\n");
 		return (3); //permission denied
 	}
-	if (!(ft_strcmp(token->token_str, ">")))
-		node->out_file = open(pathname, O_CREAT | O_RDWR | O_TRUNC, 0644);
-	else
+	if (!(ft_strcmp(token->token_str, ">>")))
 		node->out_file = open(pathname, O_CREAT | O_RDWR | O_APPEND, 0644);
+	else
+		node->out_file = open(pathname, O_CREAT | O_RDWR | O_TRUNC, 0644);
 	return (0); //no erro ?
 }
 
@@ -65,21 +70,23 @@ int	redirOut(t_sh *f, t_cmd *node, t_token *token)
 {
 	DIR	*dir;
 	char	*path;
+	char	*pathname;
 	int	i;
 
 	path = NULL;
-	path = getFilePath(f, node, token, 0);
+	path = getFilePath(f, node, token);
 	printf("path: %s\n", path);
 	if (access(path, F_OK) == 0) //path to dir exists
 	{
 		if (access(path, W_OK) == 0)
 		{
 			dir = opendir(path);
+			pathname= getFilePathName(path, f, node, token);
 			free(path);
-			path = getFilePath(f, node, token, 1);
-			i = redirecOutFile(path, node, token);
+			i = redirecOutFile(pathname, node, token);
 			closedir(dir);
-			write(node->out_file, "fode te", 7); //loladas !
+			free(pathname);
+			write(node->out_file, "1texto", 7); //!
 			return (i); //se nao for 0, erro
 		}
 		free(path);
@@ -95,6 +102,5 @@ int	parseRedirecs(t_sh *f, t_cmd *node, t_token *token)
 {
 	if (!(ft_strcmp(token->token_str, ">")) || !(ft_strcmp(token->token_str, ">>")));
 		return (redirOut(f, node, token->next));
-	if (!(ft_strcmp(token->token_str, "<")));
-		return (redirOut(f, node, token->prev));
+	return (0);
 }

6472f677d3800036bc522b11d808f00e292e6cbb
gde <goncalopes98@gmail.com>
2022-12-18 22:04:06 +0000

    redirOutput

--- src/redirecs.c
+++ src/redirecs.c
@@ -6,7 +6,7 @@
 /*   By: jdias-mo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/16 02:04:22 by gde-alme          #+#    #+#             */
-/*   Updated: 2022/12/18 21:51:02 by gde-alme         ###   ########.fr       */
+/*   Updated: 2022/12/18 22:03:35 by gde-alme         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -108,13 +108,19 @@ char	*heredoc_nstr(char *str, char *buffer)
 int	heredocfd(t_cmd *node, char *str)
 {
 	int	fd[2];
+	char	*tmp;
 
+	tmp = NULL;
 	if (node->in_file != STDIN_FILENO && node->in_file != -2 && node->in_file != -1)
 		close(node->in_file);
 	if (pipe(fd) == -1)
 		return (g_status = errno);
 	if (str)
 	{
+		tmp = ft_strjoin(str, "\n");
+		free(str);
+		str = ft_strdup(tmp);
+		free(tmp);
 		write(fd[WRITE], str, ft_strlen(str));
 		free(str);
 		close(fd[WRITE]);
@@ -139,7 +145,7 @@ int	redir_heredoc(t_cmd *node, char *eof)
 		buffer = readline("> ");
 		if (!buffer)
 			return (node->in_file = -2); //if empty
-		if (str)
+		if (str && ft_strcmp(buffer, eof) != 0)
 			str = heredoc_nstr(str, buffer);
 		else if (ft_strcmp(buffer, eof) != 0)
 			str = ft_strdup(buffer);

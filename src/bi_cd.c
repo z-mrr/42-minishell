3e0fb73c3da0ad6e503e0dd5ff08c727c7f9f89d
zet1r <zet1r@proton.me>
2022-12-19 00:22:32 +0000

    final fix cd dash

--- src/bi_cd.c
+++ src/bi_cd.c
@@ -6,7 +6,7 @@
 /*   By: jdias-mo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/18 04:56:44 by jdias-mo          #+#    #+#             */
-/*   Updated: 2022/12/19 00:15:38 by jdias-mo         ###   ########.fr       */
+/*   Updated: 2022/12/19 00:21:31 by jdias-mo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,28 +16,23 @@ int	cd_dash(t_sh *sh, t_cmd *cmd)
 {
 	char	*temp;
 
-	if (cmd->full_cmd[1] && !ft_strncmp(cmd->full_cmd[1], "-", 1) && ft_strlen(cmd->full_cmd[1]) == 1)
+	temp = cmd->full_cmd[1];
+	cmd->full_cmd[1] = get_env("OLDPWD", sh);
+	free(temp);
+	if (!cmd->full_cmd[1])
+		return(p_error("minishell: cd: OLDPWD not set", NULL, NULL, NULL, 1));
+	temp =  getcwd(NULL, 0);
+	if (chdir(cmd->full_cmd[1]) == -1)
 	{
-		temp = cmd->full_cmd[1];
-		cmd->full_cmd[1] = get_env("OLDPWD", sh);
 		free(temp);
-		if (!cmd->full_cmd[1])
-			return(p_error("minishell: cd: OLDPWD not set", NULL, NULL, NULL, 1));
-		temp =  getcwd(NULL, 0);
-		if (chdir(cmd->full_cmd[1]) == -1)
-		{
-			free(temp);
-			return(p_error("minishell: ", cmd->full_cmd[1], ": ", strerror(errno), 1));
-		}
-		set_env("OLDPWD", temp, sh);
-		free (temp);
-		temp = getcwd(NULL, 0);
-		set_env("PWD", temp, sh);
-		free (temp);
-		g_status = 0;
-		return (1);
+		return(p_error("minishell: ", cmd->full_cmd[1], ": ", strerror(errno), 1));
 	}
-	return (0);
+	set_env("OLDPWD", temp, sh);
+	free (temp);
+	temp = getcwd(NULL, 0);
+	set_env("PWD", temp, sh);
+	free (temp);
+	return (g_status = 0);
 }
 
 /*cd with only a relative or absolute path*/
@@ -52,8 +47,8 @@ int	ft_cd(t_sh *sh, t_cmd *cmd)
 		return (p_error("minishell: cd: too many arguments", NULL, NULL, NULL, 1));
 	if (n == 1)
 		cd_home(sh, cmd);
-	if (cd_dash(sh, cmd))
-		return(g_status);
+	if (cmd->full_cmd[1] && !ft_strncmp(cmd->full_cmd[1], "-", 1) && ft_strlen(cmd->full_cmd[1]) == 1)
+		return(cd_dash(sh, cmd));
 	oldpwd = getcwd(NULL, 0);
 	if (chdir(cmd->full_cmd[1]) == -1)
 	{

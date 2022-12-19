b64995ce5aca6b1ea2e1a25a5af85b655c114973
zet1r <zet1r@proton.me>
2022-12-19 00:02:23 +0000

    cd dash fix

--- src/bi_cd.c
+++ src/bi_cd.c
@@ -6,7 +6,7 @@
 /*   By: jdias-mo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/18 04:56:44 by jdias-mo          #+#    #+#             */
-/*   Updated: 2022/12/18 20:50:35 by jdias-mo         ###   ########.fr       */
+/*   Updated: 2022/12/19 00:01:56 by jdias-mo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,16 @@ int	cd_dash(t_sh *sh, t_cmd *cmd)
 {
 	char	*temp;
 
-	if (cmd->full_cmd[1] && !ft_strncmp(cmd->full_cmd[1], "-", 1))
+	if (cmd->full_cmd[1] && !ft_strncmp(cmd->full_cmd[1], "-", 1) && ft_strlen(cmd->full_cmd[1]) == 1)
 	{
 		temp = cmd->full_cmd[1];
 		cmd->full_cmd[1] = get_env("OLDPWD", sh);
 		free(temp);
 		if (!cmd->full_cmd[1])
-			return (0);
+			return(p_error("minishell: cd: OLDPWD not set", NULL, NULL, NULL, 1));
+		ft_putendl_fd(cmd->full_cmd[1], STDOUT_FILENO);
 	}
-	return (1);
+	return (0);
 }
 
 /*cd with only a relative or absolute path*/
@@ -39,13 +40,16 @@ int	ft_cd(t_sh *sh, t_cmd *cmd)
 		return (p_error("minishell: cd: too many arguments", NULL, NULL, NULL, 1));
 	if (n == 1)
 		cd_home(sh, cmd);
-	if (!cd_dash(sh, cmd))
-		return(p_error("minishell: cd: OLDPWD not set", NULL, NULL, NULL, 1));
-	if(chdir(cmd->full_cmd[1]) == -1)
+	if (cd_dash(sh, cmd))
+		return(g_status);
+	oldpwd = getcwd(NULL, 0);
+	if (chdir(cmd->full_cmd[1]) == -1)
+	{
+		free (oldpwd);
 		return (p_error("minishell: ", cmd->full_cmd[1], ": ", strerror(errno), 1));
+	}
 	pwd = getcwd(NULL, 0);
 	set_env("PWD", pwd, sh);
-	oldpwd = get_env("PWD", sh);
 	set_env("OLDPWD", oldpwd, sh);
 	free(pwd);
 	free(oldpwd);

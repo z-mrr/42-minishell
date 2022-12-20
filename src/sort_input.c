88661ab25d65a5e1a68201e4531b4ba55882d2aa
zet1r <zet1r@proton.me>
2022-12-20 00:44:22 +0000

    typo

--- src/sort_input.c
+++ src/sort_input.c
@@ -6,7 +6,7 @@
 /*   By: jdias-mo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/10 18:16:40 by gde-alme          #+#    #+#             */
-/*   Updated: 2022/12/19 22:47:43 by jdias-mo         ###   ########.fr       */
+/*   Updated: 2022/12/20 00:43:59 by jdias-mo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ static int	check_operators(t_sh *f)
 				|| (node->prev == NULL && ft_strcmp(node->word, "<<") != 0))
 			{
 				g_status = 2;
-				printf("minishell: syntax errornear unexpected token '%s'\n",
+				printf("minishell: syntax error near unexpected token '%s'\n",
 					node->word);
 				return (1);
 			}
